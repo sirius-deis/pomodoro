7c436d083f5771de0558a4b4af39ecbb3ab87b95
Sirius Deis <falebsy@gmail.com>
2023-05-12 08:04:14 +0200

    show add form on clicking add btn and hiding it on cancel click

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import './assets/style.scss';
 import timer from './timer';
+import taskList from './taskList';
 
 timer.init();
+taskList.init();
--- src/taskList.ts
+++ src/taskList.ts
@@ -9,6 +9,7 @@ class TaskList implements ITaskList {
 
     private taskList: HTMLUListElement;
     private tasksAdd: HTMLButtonElement;
+    private addForm: HTMLFormElement;
     constructor() {
         this.tasks = [];
         const tasksEl = document.querySelector('.tasks')!;
@@ -18,7 +19,31 @@ class TaskList implements ITaskList {
         this.tasksAdd = tasksEl.querySelector(
             '.tasks__add'
         ) as HTMLButtonElement;
+        this.addForm = (
+            tasksEl.querySelector('#form__template') as HTMLTemplateElement
+        ).content
+            .querySelector('.form')
+            ?.cloneNode(true) as HTMLFormElement;
     }
+
+    init = () => {
+        this.tasksAdd.addEventListener('click', this.showForm.bind(this));
+        (
+            this.addForm.querySelector(
+                '.form__btn--cancel'
+            ) as HTMLButtonElement
+        ).addEventListener('click', this.hideForm.bind(this));
+    };
+
+    private showForm = () => {
+        this.tasksAdd.replaceWith(this.addForm);
+    };
+
+    private hideForm = () => {
+        this.addForm.replaceWith(this.tasksAdd);
+    };
 }
 
-export default TaskList;
+const taskList = new TaskList();
+
+export default taskList;
